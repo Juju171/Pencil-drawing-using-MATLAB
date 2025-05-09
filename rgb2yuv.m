50ae110bb2441ffd02c92c3b9a1a6c5a5cfd88ae
BuildTools <unconfigured@null.spigotmc.org>
2025-05-09 21:52:01 +0200

    rgb2yuv and yuv2rgb modification

--- rgb2yuv.m
+++ rgb2yuv.m
@@ -1,17 +1,14 @@
-% Fonction pour convertir RGB à YUV
 function yuv = rgb2yuv(rgb)
-    % Assurez-vous que l'image est dans l'intervalle [0, 1]
+    % Make sure the image is in the range [0, 1]
     if max(rgb(:)) > 1
-        rgb = double(rgb) / 255; % Normaliser si nécessaire
+        rgb = double(rgb) / 255; % Normalize if necessary
     end
 
-    % Matrice de transformation
+    % Transform matrix
     transformation_matrix = [0.299, 0.587, 0.114;
                              -0.14713, -0.28886, 0.436;
                              0.615, -0.51499, -0.10001];
 
-    % Appliquer la transformation
-    yuv = reshape(...
-        (transformation_matrix * reshape(rgb, [], 3)')', ...
-        size(rgb, 1), size(rgb, 2), 3);
+    % Transform
+    yuv = reshape((transformation_matrix * reshape(rgb, [], 3)')', size(rgb, 1), size(rgb, 2), 3);
 end
