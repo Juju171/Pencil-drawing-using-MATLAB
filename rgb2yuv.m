eb6caf26a6a40cc1377752ae0d209c8f81a3cd56
BuildTools <unconfigured@null.spigotmc.org>
2025-05-09 21:56:20 +0200

    rgb2yuv modification

--- rgb2yuv.m
+++ rgb2yuv.m
@@ -1,7 +1,7 @@
-function yuv = rgb2yuv(img)
+function yuv = rgb2yuv(rgb)
     % Make sure the image is in the range [0, 1]
-    if max(img(:)) > 1
-        img = double(img) / 255; % Normalize if necessary
+    if max(rgb(:)) > 1
+        rgb = double(rgb) / 255; % Normalize if necessary
     end
 
     % Transform matrix
@@ -10,5 +10,5 @@ function yuv = rgb2yuv(img)
                              0.615, -0.51499, -0.10001];
 
     % Transform
-    yuv = reshape((transformation_matrix * reshape(img, [], 3)')', size(img, 1), size(img, 2), 3);
+    yuv = reshape((transformation_matrix * reshape(rgb, [], 3)')', size(rgb, 1), size(rgb, 2), 3);
 end
